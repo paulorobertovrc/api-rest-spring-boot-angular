0c6d02da368fbaf927c42758556f2f36f20811f2
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-06 12:27:56 -0400

    Created constructor

--- api/src/main/java/com/prvrc/api/product/ProductDto.java
+++ api/src/main/java/com/prvrc/api/product/ProductDto.java
@@ -3,5 +3,7 @@ package com.prvrc.api.product;
 import java.math.BigDecimal;
 
 public record ProductDto(int id, String nome, String marca, String modelo, String categoria, BigDecimal preco, int estoque) {
-    
+    public ProductDto(Product product) {
+        this(product.getId(), product.getNome(), product.getMarca(), product.getModelo(), product.getCategoria(), product.getPreco(), product.getEstoque());
+    }
 }
