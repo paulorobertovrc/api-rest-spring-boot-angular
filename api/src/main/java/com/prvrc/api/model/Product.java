74d28fdb49673cd0bfe25e404a185d10247d76eb
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-06 07:23:59 -0400

    Implementation of the Product model class

---
+++ api/src/main/java/com/prvrc/api/model/Product.java
@@ -0,0 +1,39 @@
+package com.prvrc.api.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tbProdutos")
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String produto;
+    
+    @Column(nullable = false)
+    private String marca;
+    
+    @Column(nullable = true)
+    private String modelo;
+    
+    @Column(nullable = false)
+    private String categoria;
+    
+    @Column(nullable = false)
+    private BigDecimal preco;
+    
+    @Column(nullable = true)
+    private int estoque;
+}
