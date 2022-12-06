706caf2401d7ce04d6d2d48a27162e0b304573ed
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-06 12:27:08 -0400

    Updated create and list methods

--- api/src/main/java/com/prvrc/api/controller/ProductController.java
+++ api/src/main/java/com/prvrc/api/controller/ProductController.java
@@ -1,23 +1,19 @@
 package com.prvrc.api.controller;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.prvrc.api.model.Product;
-import com.prvrc.api.repository.ProductRepository;
+import com.prvrc.api.product.Product;
+import com.prvrc.api.product.ProductDto;
+import com.prvrc.api.product.ProductRepository;
 
-import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -25,27 +21,25 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ProductController {
 
+    @Autowired
     private final ProductRepository productRepository;
 
     // Called when request method is POST
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody Product productData) {
-        // Returns http status code 201 if object is successfully created
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productData));
+    public void create(@RequestBody ProductDto productData) {
+        productRepository.save(new Product(productData));
     }
 
     // Called when request method is GET
     // This method lists all products
     @GetMapping
-    public List<Product> list() {
-        return productRepository.findAll();
+    public List<ProductDto> list() {
+        return productRepository.findAll().stream().map(ProductDto::new).toList();
     }
 
     // This method lists an specific product
-    // ***** ENCONTRAR UMA SOLUÇÃO PARA TRATAR O RETORNO NULL CASO DÊ PROBLEMA NO FRONTEND *****
     @GetMapping("/{id}")
-    @Nullable
-    public Optional<Product> findById(@PathVariable("id") int id) {
-        return productRepository.findById(id);
+    public ProductDto findById(@PathVariable("id") int id) {
+        return null;
     }
 }
