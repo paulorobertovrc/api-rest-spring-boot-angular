1877895626a99e98e88e0f105d6259b9cb003a73
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-07 07:24:56 -0400

    New dto class to be used with update method

---
+++ api/src/main/java/com/prvrc/api/product/ProductDtoUpdate.java
@@ -0,0 +1,14 @@
+package com.prvrc.api.product;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductDtoUpdate(
+            @NotNull Long id, 
+            @DecimalMin(value = "0.0") @Digits(integer = 6, fraction = 2) BigDecimal preco, 
+            int estoque) {
+
+}
