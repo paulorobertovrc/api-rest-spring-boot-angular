42b9c253b0d19b617faf51884738e0512c24bb4e
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-06 22:10:11 -0400

    Added @NotBlank, @DecimalMin and @Digits annotations

--- api/src/main/java/com/prvrc/api/product/ProductDto.java
+++ api/src/main/java/com/prvrc/api/product/ProductDto.java
@@ -2,9 +2,18 @@ package com.prvrc.api.product;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 
-public record ProductDto(Long id, @NotBlank String nome, @NotBlank String marca, String modelo, @NotBlank String categoria, @NotBlank BigDecimal preco, int estoque) {
+public record ProductDto(Long id, 
+            @NotBlank String nome, 
+            @NotBlank String marca, 
+            String modelo, 
+            @NotBlank String categoria,
+            @DecimalMin(value = "0.0") @Digits(integer = 6, fraction = 2) BigDecimal preco, 
+            int estoque) {
+    
     public ProductDto(Product product) {
         this(product.getId(), product.getNome(), product.getMarca(), product.getModelo(), product.getCategoria(), product.getPreco(), product.getEstoque());
     }
