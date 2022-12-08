96568507074b8a1ec71bdf84109903b590f5ef05
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-08 09:39:43 -0400

    Added MatTableModule

--- front/src/app/products/products.module.ts
+++ front/src/app/products/products.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
@@ -11,7 +12,8 @@ import { ProductsComponent } from './products/products.component';
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MatTableModule
   ]
 })
 export class ProductsModule { }
