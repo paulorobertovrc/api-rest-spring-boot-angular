57315c632ede115a519a559b855175f9e0ee5060
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-08 14:58:21 -0400

    Removed ununsed import

--- api/src/main/java/com/prvrc/api/controller/ProductController.java
+++ api/src/main/java/com/prvrc/api/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.prvrc.api.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
