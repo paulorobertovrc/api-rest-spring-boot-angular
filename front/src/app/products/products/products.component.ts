1b004c66832b0eaef92aabdcedad29bbe4188be4
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-08 09:41:46 -0400

    Added columns to be displayed

--- front/src/app/products/products/products.component.ts
+++ front/src/app/products/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from '../model/product';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products: Product[] = [
+  ];
+  displayedColumns = ['_id', 'category', 'inventory', 'brand', 'model', 'name', 'price'];
 }
