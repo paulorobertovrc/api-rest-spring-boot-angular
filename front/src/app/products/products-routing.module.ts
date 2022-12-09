b273ad26a7efae5adc85cc3f84118f1d5e26ba35
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-09 10:34:13 -0400

    Created NewProductFormComponent route

--- front/src/app/products/products-routing.module.ts
+++ front/src/app/products/products-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NewProductFormComponent } from './new-product-form/new-product-form.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ProductsComponent
-  }
+  { path: '', component: ProductsComponent },
+  { path: 'new', component: NewProductFormComponent }
 ];
 
 @NgModule({
