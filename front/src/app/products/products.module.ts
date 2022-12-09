e3ece7ad40022251a0362f2c5e1005d74e665a15
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-09 10:34:38 -0400

    New imports

--- front/src/app/products/products.module.ts
+++ front/src/app/products/products.module.ts
@@ -1,20 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { NewProductFormComponent } from './new-product-form/new-product-form.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
 
+
+
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    NewProductFormComponent
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
     MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
     MatToolbarModule
   ]
 })
