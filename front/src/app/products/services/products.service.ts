c5a6f75b88d080a2eebb02cdd7406902270b9b72
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-13 06:00:49 -0300

    Removed unused imports

--- front/src/app/products/services/products.service.ts
+++ front/src/app/products/services/products.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import Big from 'big.js';
-import { first } from 'rxjs';
 
 import { Product } from '../model/product';
 
